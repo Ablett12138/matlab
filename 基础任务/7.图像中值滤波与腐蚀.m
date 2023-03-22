585491580c465c4eb11d0ee15c724d18d916abe0
blance jay <48267453+Ablett12138@users.noreply.github.com>
2023-03-22 09:41:18 +0800

    up

--- 基础任务/7.图像中值滤波与腐蚀.m
+++ 基础任务/7.图像中值滤波与腐蚀.m
@@ -9,7 +9,7 @@ img = imread('fankuaituzaosheng.bmp');
 matgary=rgb2gray(img);
 
 % 使用3x3的中值滤波器进行滤波
-img_medfilt = medfilt2(matgary, [3, 3]);
+img_medfilt = medfilt2(matgary, [3, 1]);
 
 % 定义腐蚀结构元素，这里使用3x3的矩形
 SE = strel('rectangle',[3 3]);
@@ -20,19 +20,12 @@ dilated_img = imdilate(img_medfilt, SE);
 eroded_img = imerode(dilated_img, SE);
 
 %----阈值分割
-mygray_thre=function3(eroded_img,10);
+mygray_thre=function3(img_medfilt,128);
 
-%计算面积
-area=0;
-[width,height,bmsize] = size(mygray_thre);%获得图片尺寸
-for i=1:width
-    for j=1:height
-           if mygray_thre(i,j)==0
-               area=area+1;
-           end
-    end
-end
-fprintf('图形面积为:%d piex\n',area);
+area_label=area_cal(mygray_thre);
+fprintf('面积1号为:%d piex\n',area_label(1));
+fprintf('面积2号为:%d piex\n',area_label(2));
+
 
 % 在同一窗口中显示3个图像
 figure% 打开一个新的窗口
@@ -47,6 +40,8 @@ title("2.滤波后的图像");
 subplot(1,3,3);
 imshow(mygray_thre);
 title("3.阈值分割后的图像");
+
+%%%%--------------------------------------------------------   函数阈值分割
 
 %阈值分割
 function [img] = function3(img,num)
@@ -63,7 +58,8 @@ function [img] = function3(img,num)
 return
 end
 
-%%中值滤波
+%%%%--------------------------------------------------------   函数中值滤波
+
 function output_img = median_filter(input_img, filter_size)
 % input_img为输入图像，filter_size为滤波器大小
 % output_img为输出图像
@@ -104,8 +100,8 @@ output_img = uint8(output_img);
 end
 
 
+%%%%--------------------------------------------------------   函数均值滤波
 
-%%--- 均值滤波
 function output_img = mean_filter(input_img, filter_size)
 % input_img为输入图像，filter_size为滤波器大小
 % output_img为输出图像
@@ -141,3 +137,44 @@ end
 % 将输出图像转换为uint8类型
 output_img = uint8(output_img);
 end
+
+%%%%--------------------------------------------------------   函数blob连通体计算
+
+function  area_label = area_cal(gray)
+%%面积计算
+%%规避边界，防止溢出
+biaoqian = 0;
+[width,height,bmgs]=size(gray); %获得图像的尺寸：宽度，高度
+for i=2:width %行扫描
+    for j=2:height %列扫描
+        if gray(i,j) == 0 % if( (当前数据 = 黑色)
+        % if( (左边数据没标签)&&（上面数据没标签）&&（右边数据没标签）&&(下面数据没标签) )
+            if( ((gray(i,j-1)==0)||(gray(i,j-1)==255))&&((gray(i-1,j)==0)||(gray(i-1,j)==255))&&((gray(i,j+1)==0)||(gray(i,j+1)==255))&&((gray(i+1,j)==0)||(gray(i+1,j)==255)) )
+            %贴新标签
+                biaoqian = biaoqian +1;
+                gray(i,j) = biaoqian;
+            %左边坐标有标签，则改点赋值为左边标签
+            elseif( (gray(i,j-1)~=0)&&(gray(i,j-1)~=255))
+                gray(i,j) = gray(i,j-1);
+            elseif( (gray(i-1,j)~=0)&&(gray(i-1,j)~=255)) %上面坐标有标签，则改点赋值为上面标签
+                gray(i,j) = gray(i-1,j);
+            elseif( (gray(i,j+1)~=0)&&(gray(i,j+1)~=255)) %右边坐标有标签，则改点赋值为右边标签
+                gray(i,j) = gray(i,j+1);
+            elseif( (gray(i+1,j)~=0)&&(gray(i+1,j)~=255)) %下面坐标有标签，则改点赋值为下面标签
+                gray(i,j) = gray(i+1,j);
+            end
+        end
+    end
+end
+%%贴完的标签的面积计算
+area_label =zeros(254,1);
+value =0;
+for i=1:width %行扫描
+    for j=1:height %列扫描
+        if( gray(i,j)~=0 &&(gray(i,j)~=255))
+            value = gray(i,j);
+            area_label(value,1) = area_label(value,1)+1;
+        end
+    end
+end
+return
